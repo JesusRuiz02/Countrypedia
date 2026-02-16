59453f0251d5a38d58ee181a9ab73cd715f809bf
JesusRuiz02 <79652924+JesusRuiz02@users.noreply.github.com>
2026-02-15 22:59:30 -0600

    changing colors

--- app/src/main/java/com/jesusruiz/countrypedia/ui/theme/Color.kt
+++ app/src/main/java/com/jesusruiz/countrypedia/ui/theme/Color.kt
@@ -6,6 +6,14 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
+val Purple40 = Color(0xFFB0A1D5)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LightBlue = Color(0xFFA8A8A8)
+
+val Parchment = Color(0xFFF3F2F2)
+val CarbonBlack = Color(0xFF181f1c)
+val JetBlack = Color(0xFF1F2023)
+val Taupe = Color(0xFF2F3134)
+
--- app/src/main/java/com/jesusruiz/countrypedia/ui/theme/Theme.kt
+++ app/src/main/java/com/jesusruiz/countrypedia/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.jesusruiz.countrypedia.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,15 +11,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = Purple40,
+    surface = Taupe,
+    onSurface = Parchment ,
+    background = JetBlack,
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    surface = Parchment,
+    onSurface = CarbonBlack ,
+    background = LightBlue,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,7 +42,7 @@ private val LightColorScheme = lightColorScheme(
 fun CountrypediaTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
